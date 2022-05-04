7d6e36db8a1e190c99b0c485ff16e7db392143ed
Kirill <kraynov.kirill2015@yandex.ru>
2022-05-04 23:22:54 +0300

    next attempt to fix problem...... failed

--- Kafka 1/consumer/consumer.py
+++ Kafka 1/consumer/consumer.py
@@ -1,8 +1,8 @@
 from kafka import KafkaConsumer
 import time
 
-# consumer = KafkaConsumer('test', group_id='new_group', bootstrap_servers=['broker:9092'])
-consumer = KafkaConsumer('test', group_id='new_group', bootstrap_servers=['localhost:9092'])
+# consumer = KafkaConsumer('test', group_id='new_group', bootstrap_servers=['localhost:9092'])
+consumer = KafkaConsumer('test', group_id='new_group', bootstrap_servers=['broker:29092'])
 
 with open('message.log', 'w', encoding='utf-8') as file:
     for msg in consumer:
--- Kafka 1/producer/producer.py
+++ Kafka 1/producer/producer.py
@@ -17,8 +17,8 @@ def connect_kafka_producer():
     _producer = None
     try:
         print("Try create producer")
-        # _producer = KafkaProducer(bootstrap_servers=['broker:9092'], api_version=(0, 10))
-        _producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10))
+        # _producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10))
+        _producer = KafkaProducer(bootstrap_servers=['broker:29092'], api_version=(0, 10))
         print("Create producer")
     except Exception as ex:
         print('Exception while connecting Kafka')
@@ -34,4 +34,5 @@ if __name__ == '__main__':
 
         for i in range(20):
             publish_message(producer, 'test', '123', i.__str__())
+            file.write("push message")
             time.sleep(3)
